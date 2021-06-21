7e0c3b09ed565578cd668f529ea60b96e4eab881
lsm <lsm23@yandex.ru>
2021-06-21 20:23:19 +0300

    update files

--- Lession4/main.c
+++ Lession4/main.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
-#include <stdbool.h>
-
-
- bool isPrime(int n) {
-    
-    if (n > 1)
-    {
-            for (int i = 2; i < n; i++)
-            if (n % i == 0) 
-        return false;
-        return true;
-    }
-    else 
-        return false;
-} 
-
-    int main() {
-      
-    int n;
-    
-    printf("Введите число: ");
-    scanf("%d", &n);
  
-    // выводим информацию о том, простое ли число n: 1 - да, 0 - нет
-    if (isPrime(n)>0)
-    {
-    printf("Введенное число простое \n");
+void myBin(int number)
+{
+    if (number != 0) {
+        myBin(number / 2);
+        printf("%i", number % 2);
     }
-    else {
-    printf("Введенное число не простое\n");
-    }
+}
+ 
+int main () {
+    int numb;
+    //system("chcp 1251 > nul");
+ 
+    printf("Введите число в десятичном виде: "); 
+    scanf("%d", &numb);
+    printf("Число в двоичном представлении:"); 
+    myBin(numb);
+    printf("\n");
+    //system("pause > nul");
+    
+    
+    
     return 0;
 }
-    
-
-
